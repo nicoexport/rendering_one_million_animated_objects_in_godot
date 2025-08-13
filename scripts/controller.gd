eb4686533e1c50be76a36cce13188fe0825dd7e7
Nico Auerswald <auersw.nico@gmail.com>
2025-08-13 17:44:55 +0200

    implement spacing

--- scripts/controller.gd
+++ scripts/controller.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
-@export var grid_resolution: Vector2 = Vector2(20, 20) 
+@export var grid_resolution: Vector2i = Vector2(20, 20) 
+@export var grid_spacing: float = 1.2
 
 func _ready() -> void:
     for x in range(grid_resolution.x):
@@ -8,4 +9,4 @@ func _ready() -> void:
             var instance = MeshInstance3D.new()
             add_child(instance)
             instance.mesh = BoxMesh.new()
-            instance.global_position = Vector3(x, 0.0, z)
+            instance.global_position = Vector3(x * grid_spacing, 0.0, z * grid_spacing)
