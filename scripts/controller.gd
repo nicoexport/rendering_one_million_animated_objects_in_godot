9c05e9ec0834ebbbae66fdd8b4617094fa2ffb22
Nico Auerswald <auersw.nico@gmail.com>
2025-08-13 17:48:24 +0200

    center grid

--- scripts/controller.gd
+++ scripts/controller.gd
@@ -4,9 +4,16 @@ extends Node3D
 @export var grid_spacing: float = 1.2
 
 func _ready() -> void:
-    for x in range(grid_resolution.x):
-        for z in range(grid_resolution.y):
-            var instance = MeshInstance3D.new()
-            add_child(instance)
-            instance.mesh = BoxMesh.new()
-            instance.global_position = Vector3(x * grid_spacing, 0.0, z * grid_spacing)
+	# offset so grid is centered at (0,0,0)
+	var offset_x := grid_resolution.x * grid_spacing * 0.5
+	var offset_z := grid_resolution.y * grid_spacing * 0.5
+
+	for x in range(grid_resolution.x):
+		for z in range(grid_resolution.y):
+			var instance = MeshInstance3D.new()
+			add_child(instance)
+			instance.mesh = BoxMesh.new()
+
+			var pos_x = x * grid_spacing - offset_x
+			var pos_z = z * grid_spacing - offset_z
+			instance.global_position = Vector3(pos_x, 0.0, pos_z)
